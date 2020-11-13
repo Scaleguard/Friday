930410c5151549a550a704b7b4cd806a0d9d1a12
Opilane TTHK <opilane@tthk.ee>
2020-11-13 08:57:49 +0200

    Celebration times come on!

---
+++ Friday/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePath();
+            WriteDirectoriesPath();
+
+        }
+
+        public static void WriteFilePath()
+        {
+            
+            string directoryPath = @"C:\Users\opilane\Samples";
+
+            string Directorypath = @"C:\Users\opilane\Friday\SampleDirectories.txt";
+
+            string[] folders = Directory.GetDirectories(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(Directorypath, folders);
+        }
+
+        public static void WriteDirectoriesPath()
+        {
+
+            string directoryPath = @"C:\Users\opilane\Samples";
+
+            string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            string filepath = @"C:\Users\opilane\Friday\SampleFiles.txt";
+
+            File.WriteAllLines(filepath, files);
+        }
+    }
+}
